d1af4e2386b4f1b3962b923f83b52f5f5e4d0754
lab319 <39893937+lab319@users.noreply.github.com>
2021-08-20 20:28:29 +0800

    Add files via upload

---
+++ tumor_specific_multiclass_classifier/model_geo.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 27 13:52:11 2021
+
+@author: Lab319
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from xgboost.sklearn import XGBClassifier
+from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import label_binarize
+from scipy import interp
+from itertools import cycle
+from sklearn.metrics import accuracy_score, roc_auc_score
+
+
+tcgadata = pd.read_table("E:/cbj/three-diseases/classification/multiclassification/second/tcgasele.txt")
+tcgalabel = pd.read_table("E:/cbj/three-diseases/classification/multiclassification/second/tcgalabel.txt").values.reshape([-1, ])
+
+geodata = pd.read_table("E:/cbj/three-diseases/classification/multiclassification/second/geosele.txt")
+geolabel = pd.read_table("E:/cbj/three-diseases/classification/multiclassification/second/geolabel.txt").values.reshape([-1, ])
+
+n_classes = 3
+geo_one_hot = label_binarize(geolabel, np.arange(3))
+
+params={#'objective': 'multi:softmax',
+        'objective': 'multi:softmax',
+                   'num_class':3,
+                   'booster':'gbtree',
+                   'seed':12345}
+  
+    
+clf = XGBClassifier(**params)
+model = clf.fit(tcgadata.iloc[:,1:6], tcgalabel)
+#y_pre = model.predict_proba(geodata.iloc[:,1:6]).reshape(geolabel.shape[0], 3)
+y_pre = model.predict(geodata.iloc[:,1:6])
+#ylabel = np.argmax(y_pre, axis=1)
+acc = accuracy_score(geolabel,y_pre)
+acc_kirc = accuracy_score(geolabel[0:45],y_pre[0:45])
+acc_brca = accuracy_score(geolabel[46:85],y_pre[46:85])
+acc_thca = accuracy_score(geolabel[86:126],y_pre[86:126])
+print('ACC : %f' % acc)
+print('ACC_kirc : %f' % acc_kirc)
+print('ACC_brca : %f' % acc_brca)
+print('ACC_thca : %f' % acc_thca)
+
---
+++ tumor_specific_multiclass_classifier/model_tcga.py
@@ -0,0 +1,80 @@
+﻿
+# coding: utf-8
+
+# In[12]:
+
+
+import xgboost
+from sklearn.model_selection import GridSearchCV
+import numpy as np
+from sklearn.model_selection import train_test_split, KFold, cross_val_score
+from sklearn.metrics import accuracy_score, roc_auc_score
+import sklearn.metrics 
+import pandas as pd
+import numpy as np
+import xgboost as xgb
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.preprocessing import StandardScaler,Binarizer
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import matthews_corrcoef
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score, recall_score,f1_score
+from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import label_binarize
+from xgboost.sklearn import XGBClassifier
+
+
+data = pd.read_table("E:/cbj/three-diseases/classification/multiclassification/second/tcgadata.txt")
+data.columns = [str(i) for i in range(data.shape[1])]
+label = pd.read_table("E:/cbj/three-diseases/classification/multiclassification/second/tcgalabel.txt").values.reshape([-1, ])
+
+X_train,X_test,y_train,y_test = train_test_split(data,label,test_size=0.2,random_state = 123)
+    
+params={'objective': 'multi:softmax',
+            #'objective': 'multi:softprob',
+                   'num_class':3,
+                   'booster':'gbtree',
+                   'seed':12345}
+  
+    
+clf = XGBClassifier(**params)
+model = clf.fit(X_train, y_train)
+#y_pre = model.predict_proba(test_data).reshape(test_y.shape[0], 3)
+y_pre = model.predict(X_test)
+acc = accuracy_score(y_test,y_pre)
+print ('ACC : %f' % acc) 
+
+
+
+
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
