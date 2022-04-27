ac923ad920018b11230f0a55dbd36f62aa3123d9
Baoshan Ma <39893937+lab319@users.noreply.github.com>
2022-04-27 22:03:23 +0800

    Update RF_model.py

--- machine_learning_models/RF_model.py
+++ machine_learning_models/RF_model.py
@@ -20,12 +20,12 @@ import pandas as pd
 from scipy import interp
 import openpyxl
 
-file_1=open("KIRC_methy.txt")
+file_1=open("data.txt")
 file_1.readline()
 x=np.loadtxt(file_1)
 file_1.close()
 
-file_4=open("KIRC_label.txt")
+file_4=open("label.txt")
 file_4.readline()
 y=np.loadtxt(file_4)
 file_4.close()
